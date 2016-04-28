5c64a98095c0c6aa839c48dbcbdae67dac601e1c
Russell Scheerer <russell.scheerer@gmail.com>
2016-04-28 19:48:48 -0400

    Refactoring and better button control

--- scoreboard.ino
+++ scoreboard.ino
@@ -5,57 +5,56 @@
 // Similar to F(), but for PROGMEM string pointers rather than literals
 #define F2(progmem_ptr) (const __FlashStringHelper *)progmem_ptr
 
-// Hardware setup
+/////// Hardware setup ////////
+
+// Button Pin Setup
+#define BUTTON_PIN_1 10
+#define BUTTON_PIN_2 11
+#define BUTTON_PIN_3 12
+#define BUTTON_PIN_4 13
+
+// 16x32 LED Pin Setup
 #define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
 #define LAT A3
 #define OE  9
 #define A   A0
 #define B   A1
 #define C   A2
+// last param indicates 'double buffering'
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+const int BUTTON_DEBOUNCE_MS = 150;
+Bounce homeAddButton = Bounce(); 
+Bounce awayAddButton = Bounce(); 
+Bounce homeSubButton = Bounce(); 
+Bounce awaySubButton = Bounce();
 
-#define BUTTON_PIN_1 10
-#define BUTTON_PIN_2 11
-#define BUTTON_PIN_3 12
-#define BUTTON_PIN_4 13
-
-Bounce debouncer1 = Bounce(); 
-Bounce debouncer2 = Bounce(); 
-Bounce debouncer3 = Bounce(); 
-Bounce debouncer4 = Bounce();
-
-// Program variables
-       
+// Program variables       
 const int WINNING_SCORE = 21;
 int homeScore = 0;
 int awayScore = 0;
 bool updateDisplay = true;
+int hue = 0;
 
 /****************************************
  * Program Setup
  ****************************************/
 void setup() {
-  // Setup the first button with an internal pull-up :
   pinMode(BUTTON_PIN_1,INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance :
-  debouncer1.attach(BUTTON_PIN_1);
-  debouncer1.interval(250); // interval in ms
+  homeAddButton.attach(BUTTON_PIN_1);
+  homeAddButton.interval(BUTTON_DEBOUNCE_MS);
 
   pinMode(BUTTON_PIN_2,INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance :
-  debouncer2.attach(BUTTON_PIN_2);
-  debouncer2.interval(250); // interval in ms
+  awayAddButton.attach(BUTTON_PIN_2);
+  awayAddButton.interval(BUTTON_DEBOUNCE_MS);
 
   pinMode(BUTTON_PIN_3,INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance :
-  debouncer3.attach(BUTTON_PIN_3);
-  debouncer3.interval(250); // interval in ms
+  homeSubButton.attach(BUTTON_PIN_3);
+  homeSubButton.interval(BUTTON_DEBOUNCE_MS);
 
   pinMode(BUTTON_PIN_4,INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance :
-  debouncer4.attach(BUTTON_PIN_4);
-  debouncer4.interval(250); // interval in ms
+  awaySubButton.attach(BUTTON_PIN_4);
+  awaySubButton.interval(BUTTON_DEBOUNCE_MS);
 
   // initialize display
   matrix.begin();
@@ -69,7 +68,7 @@ void setup() {
  * Main Program Loop
  ****************************************/
 void loop() {
-  check_switches();
+  updateButtons();
 
   int currentHomeScore = homeScore;
   int currentAwayScore = awayScore;
@@ -100,8 +99,6 @@ void loop() {
   displayScoreBoardScreen();
   // Update display
   matrix.swapBuffers(false);
-  //textHue += 7;
-  //if(textHue >= 1536) textHue -= 1536;
 }
 
 /****************************************
@@ -134,42 +131,38 @@ bool gameOver() {
  * BUTTONS
  ****************************************/
 bool homeScored() {
-  return debouncer1.fell();
-//  return debouncer1.read() == LOW;
-//  return buttonPressed(homeAddButton);
+  return buttonJustPressed(homeAddButton);
 }
 
 bool awayScored() {
-  return debouncer2.fell();
-//  return debouncer2.read() == LOW;
-//  return buttonPressed(awayAddButton);
+  return buttonJustPressed(awayAddButton);
 }
 
 bool homeScoreCorrection() {
-  return debouncer3.fell();
-//  return debouncer3.read() == LOW;
-//  return buttonPressed(homeSubButton);
+  return buttonJustPressed(homeSubButton);
 }
 
 bool awayScoreCorrection() {
-  return debouncer4.fell();
-//  return debouncer4.read() == LOW;
-//  return buttonPressed(awaySubButton);
+  return buttonJustPressed(awaySubButton);
 }
 
 bool scoreResetPressed() {
-  return homeScoreCorrection() && awayScoreCorrection();
+  return buttonisHeld(homeSubButton) && buttonisHeld(awaySubButton);
 }
 
-//bool buttonPressed(int buttonIndex) {
-//  return justpressed[buttonIndex];
-////  return digitalRead(buttonPin) == HIGH;
-//}
+bool buttonJustPressed(Bounce button) {
+  return button.fell();
+}
+
+bool buttonisHeld(Bounce button) {
+  return button.read() == LOW;
+}
 
 /****************************************
  * Display Related
  ****************************************/
 void displayScoreBoardScreen() {
+  matrix.drawRect(0, 0, 32, 16, matrix.ColorHSV(hue, 255, 255, true));
     // fill the screen with 'black'
   //matrix.fillScreen(0);
   
@@ -181,6 +174,8 @@ void displayScoreBoardScreen() {
     displayScores();
   //}
   updateDisplay = false;
+  hue += 7;
+  if(hue >= 1536) hue -= 1536;
 }
 
 void displayScores() {
@@ -216,23 +211,10 @@ void clearScoresDisplay() {
   matrix.fillRect(0, 4, 32, 10, matrix.Color333(0, 0, 0));
 }
 
-// Scrolls text from right to left with changing colors
-//void displayScrollingText() {
-//  // Draw big scrolly text on top
-//  matrix.setTextColor(matrix.ColorHSV(textHue, 255, 255, true));
-//  matrix.setCursor(textX, 0); // top line
-//  matrix.print(F2(str));
-//
-//  // Move text left (w/wrap), increase hue
-//  if((--textX) < textMin) textX = matrix.width();
-//  textHue += 7;
-//  if(textHue >= 1536) textHue -= 1536;
-//}
-
-void check_switches()
+void updateButtons()
 {
-  debouncer1.update();
-  debouncer2.update();
-  debouncer3.update();
-  debouncer4.update();
+  homeAddButton.update();
+  awayAddButton.update();
+  homeSubButton.update();
+  awaySubButton.update();
 }
